ea03ab17ef1a41f335164e7795a6234355d9b656
usha <uvaswani@gmail.com>
2023-09-25 14:49:48 -0700

    first commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hello World');
+let kiku = 20;
+if (kiku < 50)
+    kiku += 10;
+console.log(kiku);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log('Hello World');
+let kiku: number = 20;
+if (kiku <50)
+    kiku +=10;
+    console.log(kiku);
